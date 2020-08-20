a59060ca8439dab8111135d7c4e02e3a9d2c2c3c
sergio <sergio.andrade@sensedata.com.br>
2020-08-19 23:28:07 -0300

    COUNTER dict

--- 1090/main.py
+++ 1090/main.py
@@ -4,6 +4,11 @@ import re
 TRIANGULO_REX = re.compile(r'triangulo?s*')
 CIRCULO_REX = re.compile(r'circulo?s*')
 QUADRADO_REX = re.compile(r'quadrado?s*')
+COUNTER = {
+    1: 1,
+    2: 0,
+    3: 1,
+}
 
 
 def value_mapper(card):
@@ -15,15 +20,6 @@ def value_mapper(card):
         return 2
 
 
-def valid_counter(cards):
-    cards_len = len(list(cards))
-
-    if cards_len == 1 or cards_len == 3:
-        return 1
-
-    return 0
-
-
 while True:
     entries = int(input())
     if entries == 0:
@@ -31,11 +27,12 @@ while True:
 
     counter_cards = []
     cards = []
+    result = 0
     for _ in range(entries):
         cards.append(value_mapper(input()))
         if len(cards) == 3:
-            counter_cards.append(valid_counter(cards))
+            result += COUNTER[len(cards)]
             cards = []
             continue
 
-    print(sum(counter_cards))
+    print(result)
